b8d563a74dc25ccb5553c57907a52093152bb6c2
aladdinVR <adrien.berger@student-cs.fr>
2022-03-26 17:36:52 +0100

    main app template

---
+++ src/utils/Props/index.d.ts
@@ -0,0 +1,13 @@
+export type FeaturedPostProps = {
+  post: {
+    date: string;
+    description: string;
+    image: string;
+    imageLabel: string;
+    title: string;
+  };
+};
+
+export type MarkdownProps = {
+  fileName: string;
+};
