5ca06bc5e9038f4aed2cdaa047282bc9ae87389b
aladdinVR <adrien.berger@student-cs.fr>
2022-04-11 22:24:10 +0200

    remove sidebar

--- src/components/ts/Home.tsx
+++ src/components/ts/Home.tsx
@@ -10,7 +10,6 @@ import Header from "./Header";
 import MainFeaturedPost from "./MainFeaturedPost";
 import FeaturedPost from "./FeaturedPost";
 import Main from "./Main";
-import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import homeEn from "../markdown/en/home.md";
 import homeFr from "../markdown/fr/home.md";
--- src/components/ts/Main.tsx
+++ src/components/ts/Main.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Markdown from "./Markdown";
 import { MainProps } from "../../utils/Props";
